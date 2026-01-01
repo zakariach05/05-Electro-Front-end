43eae3cf1c136228e5e24a2f3bf118067c2e4508
Zakaria Chamekh <chamekhzakaria95@gmail.com>
2026-01-01 13:01:23 +0100

    save 1/01/2026

--- src/services/api.js
+++ src/services/api.js
@@ -1,1 +1,11 @@
-export const API_URL = "http://localhost:8000/api";
+import axios from "axios";
+
+// URL du backend Laravel sur InfinityFree
+export const API_URL = "https://05-electro.kesug.com/api";
+
+const api = axios.create({
+    baseURL: API_URL,
+    withCredentials: true, // 🔑 nécessaire pour les cookies Sanctum
+});
+
+export default api;
