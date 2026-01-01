8f933963d077ef8e3b8ceed79b4c4879eacfb93e
Zakaria Chamekh <chamekhzakaria95@gmail.com>
2026-01-01 15:43:11 +0100

    save 1/01/2026

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 // URL du backend Laravel sur InfinityFree
-export const API_URL = "https://05-electro.kesug.com/api";
+// export const API_URL = "https://05-electro.kesug.com/api";
+export const API_URL = "https://05-electro.kesug.com/api-proxy.php";
 
 const api = axios.create({
     baseURL: API_URL,
